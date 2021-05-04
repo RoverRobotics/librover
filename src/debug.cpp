4f1bb64ea1bf20636cbae46ed701351e3f9a2101
drhieu <will@roverrobotics.com>
2021-05-04 14:05:10 -0500

    update gitignore and added debug

--- src/debug.cpp
+++ src/debug.cpp
@@ -2,19 +2,20 @@
 
 #include <memory>
 
-// #include "librover/protocol_pro_2.hpp"
+#include "librover/protocol_pro_2.hpp"
 #include "time.h"
-// namespace RoverRobotics {
-// int Rover() {
-//   Control::pid_gains testgains_ = {0, 0, 0};
-//   Control::robot_motion_mode_t robot_mode = Control::OPEN_LOOP;
-//   std::unique_ptr<BaseProtocolObject> robot_ =
-//       std::make_unique<Pro2ProtocolObject>("can0", "can", robot_mode,
-//                                            testgains_);
-// }
-// }  // namespace RoverRobotics
+namespace RoverRobotics {
+int Rover() {
+  Control::pid_gains testgains_ = {0, 0, 0};
+  Control::robot_motion_mode_t robot_mode = Control::OPEN_LOOP;
+  Control::angular_scaling_params angular_scaling_params_ = {0, 0, 0, 0, 0};
+  std::unique_ptr<BaseProtocolObject> robot_ = std::make_unique<Pro2ProtocolObject>(
+        "can0", "can", robot_mode, testgains_,
+        angular_scaling_params_);
+}
+}  // namespace RoverRobotics
 int main() {
-  //int rover = RoverRobotics::Rover();
+  int rover = RoverRobotics::Rover();
   while (true) {
     continue;
   }
