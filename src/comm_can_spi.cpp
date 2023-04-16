e2b7f20c9149577bc2e549613a67ddb37dbe14ed
Jack Rivera <jackarivera@gmail.com>
2023-04-16 16:15:20 -0400

    test

--- src/comm_can_spi.cpp
+++ src/comm_can_spi.cpp
@@ -70,10 +70,10 @@ CommCanSPI::CommCanSPI(const char *device, std::function<void(std::vector<uint8_
 }
 
 void CommCanSPI::write_to_device(std::vector<uint8_t> msg) {
-  std::cout << "Expected CAN message: ";
-  for (int i = 0; i < CAN_MSG_SIZE_; i++) {
-    std::cout << std::hex << static_cast<int>(msg[i]) << " ";
-  }
+  // std::cout << "Expected CAN message: ";
+  // for (int i = 0; i < CAN_MSG_SIZE_; i++) {
+  //   std::cout << std::hex << static_cast<int>(msg[i]) << " ";
+  // }
   std::cout << std::endl;
   Can_write_mutex_.lock();
   if (msg.size() == CAN_MSG_SIZE_) {
