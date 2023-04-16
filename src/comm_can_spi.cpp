cb72bdc02f9e9eb354543561620633145cb81c2b
Jack Rivera <jackarivera@gmail.com>
2023-04-16 14:39:05 -0400

    froze?

--- src/comm_can_spi.cpp
+++ src/comm_can_spi.cpp
@@ -36,10 +36,10 @@ CommCanSPI::CommCanSPI(const char *device, std::function<void(std::vector<uint8_
     0x00
   };
   ftdi_write_data(ftdi, spi_read_canctrl, 3);
-
+  printf("Wrote read command for CANCTRL\n");
   unsigned char spi_read_buffer[3];
   ftdi_read_data(ftdi, spi_read_buffer, 3);
-  printf("CANCTRL Register: 0x%x", spi_read_buffer[2]);
+  printf("CANCTRL Register: 0x%x\n", spi_read_buffer[2]);
 
   /*
   // configure SPI bus
