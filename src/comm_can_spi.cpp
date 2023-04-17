3de87117bd4b37ad545a5af3009faf310352c34d
Jack Rivera <jackarivera@gmail.com>
2023-04-16 20:11:58 -0400

    fix read bytes

--- src/comm_can_spi.cpp
+++ src/comm_can_spi.cpp
@@ -64,14 +64,16 @@ CommCanSPI::CommCanSPI(const char *device, std::function<void(std::vector<uint8_
   }
   // Read SPI response from device
   unsigned char spi_read_buffer[1];
-  //int r = ftdi_read_data(ftdi, spi_read_buffer, sizeof(spi_read_buffer));
+  int r = ftdi_read_data(ftdi, spi_read_buffer, sizeof(spi_read_buffer));
+  printf("SPI Read Bytes: %d\n", r);
+  /*
   if (ftdi_read_data(ftdi, spi_read_buffer, 1) != 1) {
       fprintf(stderr, "Failed to read SPI response: %s\n", ftdi_get_error_string(ftdi));
       ftdi_usb_close(ftdi);
       ftdi_deinit(ftdi);
       throw(OPEN_DEVICE_FAIL);
   }
-  
+  */
   printf("CANCTRL Register: 0x%02x\n", spi_read_buffer[0]);
   /*
   for (int i = 0; i < r; i++) {
