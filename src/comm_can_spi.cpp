321433b45d80a23a46c77e5742d599c34ec2dd4c
Jack Rivera <jackarivera@gmail.com>
2023-04-23 19:01:54 -0400

    test

--- src/comm_can_spi.cpp
+++ src/comm_can_spi.cpp
@@ -16,8 +16,8 @@ CommCanSPI::CommCanSPI(const char *device, std::function<void(std::vector<uint8_
   char conf_can_cmd[] = {
     MCP_CMD_WRITE,
     0b00101000,
-    0x05,
-    0xB5,
+    0x84,
+    0xF6,
     0x41
   };
 
