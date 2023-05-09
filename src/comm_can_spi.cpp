6f47defc3cf7293843a32216b5a6df5c3b614f9d
Jackarivera <jack@roverrobotics.com>
2023-05-09 10:28:32 -0500

    reimplement read

--- src/comm_can_spi.cpp
+++ src/comm_can_spi.cpp
@@ -269,10 +269,10 @@ CommCanSPI::CommCanSPI(const char *device, std::function<void(std::vector<uint8_
   } 
 
   // start read thread
-  /*
+  
   Can_read_thread_ = std::thread(
       [this, parsefunction]() { this->read_device_loop(parsefunction); });
-  */
+  
 }
 
 
